53f37205319b7a30af7ab612eb863f8fb06c9268
rampling <rampling222@163.com>
2021-02-26 22:03:25 +0800

    controller测试

--- src/main/java/com/rampling/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/rampling/o2o/web/superadmin/AreaController.java
@@ -4,7 +4,6 @@ import com.rampling.o2o.entity.Area;
 import com.rampling.o2o.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
